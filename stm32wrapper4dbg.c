bc78c2665002fd3ef4a88605eb8ac694257042b5
Antonio Borneo <antonio.borneo@st.com>
2020-12-16 17:41:33 +0100

    stm32wrapper4dbg: add header v2 support
    On top of existing header v1 support, add support for the new
    header v2.
    Detect and skip the case of encrypted content.
    Allow any header v2 size in input, while keep a constant header
    size in output.
    Minor code cleanups.
    
    Signed-off-by: Antonio Borneo <antonio.borneo@st.com>
    Change-Id: Ib520041c39acce88df36ec672ca818e9403e0e74
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/stm32wrapper4dbg/+/188757

--- stm32wrapper4dbg.c
+++ stm32wrapper4dbg.c
@@ -7,6 +7,8 @@
 #include <asm/byteorder.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,10 +25,10 @@
 #define VER_MINOR		1
 #define VER_VARIANT		0
 #define HEADER_VERSION_V1	0x1
-#define TF_BINARY_TYPE		0x10
-
-/* Default option : bit0 => no signature */
-#define HEADER_DEFAULT_OPTION	(__cpu_to_le32(0x00000001))
+#define HEADER_VERSION_V2	0x2
+#define PADDING_HEADER_MAGIC	__be32_to_cpu(0x5354FFFF)
+#define PADDING_HEADER_FLAG	(1 << 31)
+#define PADDING_HEADER_LENGTH	0x180
 
 #define ARM_THUMB_ADDRESS(a)	((a) | 1)
 #define ARM_THUMB_INSN(a)	((a) & ~1)
@@ -42,37 +44,55 @@ struct stm32_header {
 	uint32_t magic_number;
 	uint8_t image_signature[64];
 	uint32_t image_checksum;
-	uint8_t  header_version[4];
+	uint8_t header_version[4];
 	uint32_t image_length;
 	uint32_t image_entry_point;
 	uint32_t reserved1;
 	uint32_t load_address;
 	uint32_t reserved2;
 	uint32_t version_number;
-	uint32_t option_flags;
-	uint32_t ecdsa_algorithm;
-	uint8_t ecdsa_public_key[64];
-	uint8_t padding[83];
-	uint8_t binary_type;
+	union {
+		struct {
+			uint32_t option_flags;
+			uint32_t ecdsa_algorithm;
+			uint8_t ecdsa_public_key[64];
+			uint8_t padding[83];
+			uint8_t binary_type;
+			/* used to get header length */
+			uint8_t _last[0];
+		} v1;
+		struct {
+			uint32_t extension_flags;
+			uint32_t extension_headers_length;
+			uint32_t binary_type;
+			uint8_t padding[16];
+			/* input file can have other extensions */
+			struct {
+				uint32_t type;
+				uint32_t length;
+				uint8_t padding[376];
+			} extension;
+		} v2;
+	};
 };
 
-static void stm32image_default_header(struct stm32_header *ptr)
+static uint32_t stm32image_header_length(struct stm32_header *ptr)
 {
-	if (!ptr) {
-		return;
+	switch (ptr->header_version[VER_MAJOR]) {
+	case HEADER_VERSION_V1:
+		return offsetof(struct stm32_header, v1._last);
+	case HEADER_VERSION_V2:
+		return offsetof(struct stm32_header, v2.extension) +
+			__le32_to_cpu(ptr->v2.extension_headers_length);
+	default:
+		return 0;
 	}
-
-	ptr->magic_number = HEADER_MAGIC;
-	ptr->option_flags = HEADER_DEFAULT_OPTION;
-	ptr->ecdsa_algorithm = __cpu_to_le32(1);
-	ptr->version_number = __cpu_to_le32(0);
-	ptr->binary_type = TF_BINARY_TYPE;
 }
 
-static uint32_t stm32image_checksum(void *start, uint32_t len)
+static uint32_t stm32image_checksum(struct stm32_header *start, uint32_t len,
+				    uint32_t hdr_len)
 {
 	uint32_t csum = 0;
-	uint32_t hdr_len = sizeof(struct stm32_header);
 	uint8_t *p;
 
 	if (len < hdr_len) {
@@ -91,12 +111,12 @@ static uint32_t stm32image_checksum(void *start, uint32_t len)
 	return csum;
 }
 
-static int stm32image_check_hdr(void *ptr, uint32_t file_length)
+static int stm32image_check_hdr(struct stm32_header *stm32hdr,
+				uint32_t file_length)
 {
-	struct stm32_header *stm32hdr = (struct stm32_header *)ptr;
-	uint32_t img_length = file_length - sizeof(struct stm32_header);
+	uint32_t hdr_length, img_length;
 
-	if (file_length <= sizeof(struct stm32_header)) {
+	if (file_length <= offsetof(struct stm32_header, v1)) {
 		fprintf(stderr, "File too small\n");
 		return -1;
 	}
@@ -106,6 +126,18 @@ static int stm32image_check_hdr(void *ptr, uint32_t file_length)
 		return -1;
 	}
 
+	hdr_length = stm32image_header_length(stm32hdr);
+	if (hdr_length == 0) {
+		fprintf(stderr, "Unknown header version\n");
+		return -1;
+	}
+
+	if (file_length <= hdr_length) {
+		fprintf(stderr, "File too small\n");
+		return -1;
+	}
+
+	img_length = file_length - hdr_length;
 	if (__le32_to_cpu(stm32hdr->image_length) > img_length) {
 		fprintf(stderr, "Wrong image length\n");
 		return -1;
@@ -113,10 +145,10 @@ static int stm32image_check_hdr(void *ptr, uint32_t file_length)
 
 	/* There could be padding at the end of input file */
 	img_length = __le32_to_cpu(stm32hdr->image_length);
-	file_length = sizeof(struct stm32_header) + img_length;
+	file_length = hdr_length + img_length;
 
 	if (__le32_to_cpu(stm32hdr->image_checksum) !=
-	    stm32image_checksum(ptr, file_length)) {
+	    stm32image_checksum(stm32hdr, file_length, hdr_length)) {
 		fprintf(stderr, "Wrong image checksum\n");
 		return -1;
 	}
@@ -126,11 +158,25 @@ static int stm32image_check_hdr(void *ptr, uint32_t file_length)
 		return -1;
 	}
 
-	for (int i = 0; i < sizeof(stm32hdr->padding); i++) {
-		if (stm32hdr->padding[i]) {
-			fprintf(stderr, "Wrong image, padding not zero\n");
-			return -1;
+	switch (stm32hdr->header_version[VER_MAJOR]) {
+	case HEADER_VERSION_V1:
+		for (int i = 0; i < sizeof(stm32hdr->v1.padding); i++) {
+			if (stm32hdr->v1.padding[i]) {
+				fprintf(stderr, "Wrong image, padding not zero\n");
+				return -1;
+			}
 		}
+		break;
+	case HEADER_VERSION_V2:
+		for (int i = 0; i < sizeof(stm32hdr->v2.padding); i++) {
+			if (stm32hdr->v2.padding[i]) {
+				fprintf(stderr, "Wrong image, padding not zero\n");
+				return -1;
+			}
+		}
+		break;
+	default:
+		return -1;
 	}
 
 	if ((__le32_to_cpu(stm32hdr->image_entry_point) <
@@ -144,20 +190,20 @@ static int stm32image_check_hdr(void *ptr, uint32_t file_length)
 	return 0;
 }
 
-static int stm32image_check_wrapper(void *ptr)
+static int stm32image_check_wrapper(struct stm32_header *stm32hdr)
 {
-	struct stm32_header *stm32hdr = (struct stm32_header *)ptr;
-	uint32_t file_length, loadaddr, entry, pos;
+	uint32_t file_length, loadaddr, entry, pos, hdr_length;
 
-	file_length = sizeof(struct stm32_header) + __le32_to_cpu(stm32hdr->image_length);
+	hdr_length = stm32image_header_length(stm32hdr);
+	file_length = hdr_length + __le32_to_cpu(stm32hdr->image_length);
 	loadaddr = __le32_to_cpu(stm32hdr->load_address);
 	entry = ARM_THUMB_INSN(__le32_to_cpu(stm32hdr->image_entry_point));
 
-	pos = sizeof(struct stm32_header) + entry - loadaddr;
+	pos = hdr_length + entry - loadaddr;
 	if (pos + sizeof(stm32_wrapper) + sizeof(uint32_t) > file_length)
 		return 0;
 
-	if (memcmp(((char *)ptr) + pos, stm32_wrapper, sizeof(stm32_wrapper)))
+	if (memcmp(((char *)stm32hdr) + pos, stm32_wrapper, sizeof(stm32_wrapper)))
 		return 0;
 
 	return -1;
@@ -178,30 +224,72 @@ static void stm32image_print_header(const void *ptr)
 	       __le32_to_cpu(stm32hdr->image_entry_point));
 	printf("Checksum     : 0x%08x\n",
 	       __le32_to_cpu(stm32hdr->image_checksum));
-	printf("Option       : 0x%08x\n",
-	       __le32_to_cpu(stm32hdr->option_flags));
+
+	switch (stm32hdr->header_version[VER_MAJOR]) {
+	case HEADER_VERSION_V1:
+		printf("Option       : 0x%08x\n",
+		       __le32_to_cpu(stm32hdr->v1.option_flags));
+		break;
+
+	case HEADER_VERSION_V2:
+		printf("Extension    : 0x%08x\n",
+		       __le32_to_cpu(stm32hdr->v2.extension_flags));
+		break;
+
+	default:
+		printf("Incorrect header version\n");
+	}
+
 	printf("Version	     : 0x%08x\n",
 	       __le32_to_cpu(stm32hdr->version_number));
 }
 
-static void stm32image_set_header(void *ptr, uint32_t file_size, int ifd,
-				  uint32_t loadaddr, uint32_t ep, uint32_t ver,
-				  uint32_t major, uint32_t minor, uint8_t type)
+static int stm32image_init_header(struct stm32_header *dest_hdr,
+				  struct stm32_header *src_hdr)
+{
+	memset(dest_hdr, 0, sizeof(struct stm32_header));
+
+	dest_hdr->magic_number = HEADER_MAGIC;
+	dest_hdr->version_number = src_hdr->version_number;
+	dest_hdr->header_version[VER_MAJOR] = src_hdr->header_version[VER_MAJOR];
+	dest_hdr->header_version[VER_MINOR] = src_hdr->header_version[VER_MINOR];
+	switch (src_hdr->header_version[VER_MAJOR]) {
+	case HEADER_VERSION_V1:
+		/* Default option for header v1 : bit0 => no signature */
+		dest_hdr->v1.option_flags = __cpu_to_le32(0x00000001);
+		dest_hdr->v1.ecdsa_algorithm = __cpu_to_le32(1);
+		dest_hdr->v1.binary_type = src_hdr->v1.binary_type;
+		break;
+	case HEADER_VERSION_V2:
+		dest_hdr->v2.binary_type = src_hdr->v2.binary_type;
+		dest_hdr->v2.extension_flags =
+			__cpu_to_le32(PADDING_HEADER_FLAG);
+		dest_hdr->v2.extension_headers_length =
+			__cpu_to_le32(PADDING_HEADER_LENGTH);
+		dest_hdr->v2.extension.type = PADDING_HEADER_MAGIC;
+		dest_hdr->v2.extension.length =
+			__cpu_to_le32(PADDING_HEADER_LENGTH);
+		break;
+	default:
+		return -1;
+	}
+	return 0;
+}
+
+static int stm32image_update_header(void *ptr, uint32_t file_size,
+				    uint32_t loadaddr, uint32_t ep)
 {
 	struct stm32_header *stm32hdr = (struct stm32_header *)ptr;
+	uint32_t hdr_length;
 
-	stm32image_default_header(stm32hdr);
+	hdr_length = stm32image_header_length(stm32hdr);
 
-	stm32hdr->header_version[VER_MAJOR] = major;
-	stm32hdr->header_version[VER_MINOR] = minor;
 	stm32hdr->load_address = __cpu_to_le32(loadaddr);
 	stm32hdr->image_entry_point = __cpu_to_le32(ep);
-	stm32hdr->image_length = __cpu_to_le32(file_size -
-					     sizeof(struct stm32_header));
+	stm32hdr->image_length = __cpu_to_le32(file_size - hdr_length);
 	stm32hdr->image_checksum =
-		__cpu_to_le32(stm32image_checksum(ptr, file_size));
-	stm32hdr->version_number = __cpu_to_le32(ver);
-	stm32hdr->binary_type = type;
+		__cpu_to_le32(stm32image_checksum(stm32hdr, file_size, hdr_length));
+	return 0;
 }
 
 static int stm32image_create_header_file(char *srcname, char *destname,
@@ -211,20 +299,17 @@ static int stm32image_create_header_file(char *srcname, char *destname,
 	struct stat sbuf;
 	unsigned char *ptr;
 	struct stm32_header stm32image_header;
-	struct stm32_header *stm32hdr;
+	struct stm32_header *src_hdr;
+	uint32_t src_hdr_length, dest_hdr_length;
 	unsigned char *src_data;
 	size_t src_size, dest_size;
-	uint32_t src_length, jmp_add, padding, wrp_loadaddr, wrp_size;
+	uint32_t src_data_length, jmp_add, padding, wrp_loadaddr, wrp_size;
 	uint32_t new_loadaddr, new_entry = 0;
-	uint32_t loadaddr, entry, version, major, minor, option;
-	uint8_t type;
+	uint32_t loadaddr, entry, option;
+	bool is_signed, is_encrypted;
+	unsigned char padding_zeros[WRAPPER_ALIGNMENT];
 
-	dest_fd = open(destname, O_RDWR | O_CREAT | O_TRUNC | O_APPEND, 0666);
-	if (dest_fd == -1) {
-		fprintf(stderr, "Can't open %s: %s\n", destname,
-			strerror(errno));
-		return -1;
-	}
+	memset(padding_zeros, 0, sizeof(padding_zeros));
 
 	src_fd = open(srcname, O_RDONLY);
 	if (src_fd == -1) {
@@ -248,23 +333,24 @@ static int stm32image_create_header_file(char *srcname, char *destname,
 		fprintf(stderr, "Can't read %s\n", srcname);
 		return -1;
 	}
+	src_hdr = (struct stm32_header *)ptr;
 
-	if (stm32image_check_hdr(ptr, src_size) < 0) {
+	if (stm32image_check_hdr(src_hdr, src_size) < 0) {
 		fprintf(stderr, "Not a valid image file %s\n", srcname);
 		return -1;
 	}
 
-	stm32hdr = (struct stm32_header *)ptr;
-	src_data = ptr + sizeof(struct stm32_header);
-	src_length = src_size - sizeof(struct stm32_header);
+	src_hdr_length = stm32image_header_length(src_hdr);
+	src_data = ptr + src_hdr_length;
+	src_data_length = src_size - src_hdr_length;
 
-	if (__le32_to_cpu(stm32hdr->image_length) < src_length) {
+	if (__le32_to_cpu(src_hdr->image_length) < src_data_length) {
                 fprintf(stderr, "Strip extra padding from input file\n");
-		src_length = __le32_to_cpu(stm32hdr->image_length);
-		src_size = sizeof(struct stm32_header) + src_length;
+		src_data_length = __le32_to_cpu(src_hdr->image_length);
+		src_size = src_hdr_length + src_data_length;
 	}
 
-	if (force == 0 && stm32image_check_wrapper(ptr) < 0) {
+	if (force == 0 && stm32image_check_wrapper(src_hdr) < 0) {
 		fprintf(stderr,
 			"Wrapper already present in image file %s\n"
 			"Use flag \"-f\" to force re-adding the wrapper\n",
@@ -272,18 +358,44 @@ static int stm32image_create_header_file(char *srcname, char *destname,
 		return -1;
 	}
 
-	major = stm32hdr->header_version[VER_MAJOR];
-	minor = stm32hdr->header_version[VER_MINOR];
-	entry = __le32_to_cpu(stm32hdr->image_entry_point);
-	loadaddr = __le32_to_cpu(stm32hdr->load_address);
-	version = __le32_to_cpu(stm32hdr->version_number);
-	option = __le32_to_cpu(stm32hdr->option_flags);
-	type = stm32hdr->binary_type;
+	entry = __le32_to_cpu(src_hdr->image_entry_point);
+	loadaddr = __le32_to_cpu(src_hdr->load_address);
 
-	memset(&stm32image_header, 0, sizeof(struct stm32_header));
+	switch (src_hdr->header_version[VER_MAJOR]) {
+	case HEADER_VERSION_V1:
+		option = __le32_to_cpu(src_hdr->v1.option_flags);
+		is_signed = (option & 1) == 0;
+		is_encrypted = false;
+		break;
+	case HEADER_VERSION_V2:
+		option = __le32_to_cpu(src_hdr->v2.extension_flags);
+		is_signed = (option & 1) != 0;
+		is_encrypted = (option & 2) != 0;
+		break;
+	default:
+		return -1;
+	}
 
-	if (write(dest_fd, &stm32image_header, sizeof(struct stm32_header)) !=
-	    sizeof(struct stm32_header)) {
+	if (is_encrypted) {
+		fprintf(stderr,
+			"Image %s is encrypted. Unable to extract the content.\n",
+			srcname);
+		return -1;
+	}
+
+	dest_fd = open(destname, O_RDWR | O_CREAT | O_TRUNC | O_APPEND, 0666);
+	if (dest_fd == -1) {
+		fprintf(stderr, "Can't open %s: %s\n", destname,
+			strerror(errno));
+		return -1;
+	}
+
+	if (stm32image_init_header(&stm32image_header, src_hdr) < 0)
+		return -1;
+
+	dest_hdr_length = stm32image_header_length(&stm32image_header);
+
+	if (write(dest_fd, &stm32image_header, dest_hdr_length) != dest_hdr_length) {
 		fprintf(stderr, "Write error %s: %s\n", destname,
 			strerror(errno));
 		return -1;
@@ -296,8 +408,8 @@ static int stm32image_create_header_file(char *srcname, char *destname,
 		padding = loadaddr - (wrp_loadaddr + wrp_size);
 		new_loadaddr = wrp_loadaddr;
 	} else {
-		wrp_loadaddr = ALIGN(loadaddr + src_length, WRAPPER_ALIGNMENT);
-		padding = wrp_loadaddr - (loadaddr + src_length);
+		wrp_loadaddr = ALIGN(loadaddr + src_data_length, WRAPPER_ALIGNMENT);
+		padding = wrp_loadaddr - (loadaddr + src_data_length);
 		new_loadaddr = loadaddr;
 	}
 
@@ -319,23 +431,21 @@ static int stm32image_create_header_file(char *srcname, char *destname,
 			return -1;
 		}
 
-		/* sizeof(stm32image_header) bigger than max padding */
-		if (write(dest_fd, &stm32image_header, padding) != padding) {
+		if (write(dest_fd, padding_zeros, padding) != padding) {
 			fprintf(stderr, "Write error %s: %s\n", destname,
 				strerror(errno));
 			return -1;
 		}
 	}
 
-	if (write(dest_fd, src_data, src_length) != src_length) {
+	if (write(dest_fd, src_data, src_data_length) != src_data_length) {
 		fprintf(stderr, "Write error on %s: %s\n", destname,
 			strerror(errno));
 		return -1;
 	}
 
 	if (wrapper_before == 0) {
-		/* sizeof(stm32image_header) bigger than max padding */
-		if (write(dest_fd, &stm32image_header, padding) != padding) {
+		if (write(dest_fd, padding_zeros, padding) != padding) {
 			fprintf(stderr, "Write error %s: %s\n", destname,
 				strerror(errno));
 			return -1;
@@ -359,7 +469,7 @@ static int stm32image_create_header_file(char *srcname, char *destname,
 	munmap((void *)ptr, src_size);
 	close(src_fd);
 
-	dest_size = sizeof(struct stm32_header) + wrp_size + padding + src_length;
+	dest_size = dest_hdr_length + wrp_size + padding + src_data_length;
 
 	ptr = mmap(0, dest_size, PROT_READ | PROT_WRITE, MAP_SHARED,
 		   dest_fd, 0);
@@ -369,13 +479,12 @@ static int stm32image_create_header_file(char *srcname, char *destname,
 		return -1;
 	}
 
-	stm32image_set_header(ptr, dest_size, dest_fd, new_loadaddr, new_entry,
-			      version, major, minor, type);
+	stm32image_update_header(ptr, dest_size, new_loadaddr, new_entry);
 
 	stm32image_print_header(ptr);
 	printf("Halt Address : 0x%08x\n", jmp_add);
 
-	if ((option & 1) == 0)
+	if (is_signed)
 		printf("\nATTENTION:\n\tSource file \"%s\" was \"signed\"!\n"
 		       "\tYou would need to sign the destination file \"%s\"\n",
 		       srcname, destname);
